08fb10608691c0fbc11d285a49460397644a0e73
sagarnarayanan <sagarnmani123@gmail.com>
2022-01-22 17:30:54 +0530

    Changed Parameters.

--- Contract_Review_BigBird/run.sh
+++ Contract_Review_BigBird/run.sh
@@ -8,11 +8,11 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
         --do_eval \
         --version_2_with_negative \
         --learning_rate 1e-4 \
-        --num_train_epochs 4 \
-        --per_gpu_eval_batch_size=10  \
-        --per_gpu_train_batch_size=10 \
+        --num_train_epochs 2 \
+        --per_gpu_eval_batch_size=2  \
+        --per_gpu_train_batch_size=2 \
         --max_seq_length 512 \
-        --max_answer_length 512 \
+        --max_answer_length 256 \
         --doc_stride 256 \
         --save_steps 1000 \
         --n_best_size 20 \
