d17a1309dd5cb172b9c178d093630b8a55a015b2
sagarnarayanan <sagarnmani123@gmail.com>
2022-01-22 16:32:39 +0530

    Initial Commit
    Initial Commit

---
+++ Contract_Review_BigBird/run.sh
@@ -0,0 +1,19 @@
+CUDA_VISIBLE_DEVICES=0,1 python train.py \
+        --output_dir ./train_models/bigbird-roberta-base \
+        --model_type bigbird-roberta \
+        --model_name_or_path bigbird-roberta-base \
+        --train_file ./data/train_separate_questions.json \
+        --predict_file ./data/test.json \
+        --do_train \
+        --do_eval \
+        --version_2_with_negative \
+        --learning_rate 1e-4 \
+        --num_train_epochs 4 \
+        --per_gpu_eval_batch_size=40  \
+        --per_gpu_train_batch_size=40 \
+        --max_seq_length 1024 \
+        --max_answer_length 1024 \
+        --doc_stride 256 \
+        --save_steps 1000 \
+        --n_best_size 20 \
+        --overwrite_output_dir
